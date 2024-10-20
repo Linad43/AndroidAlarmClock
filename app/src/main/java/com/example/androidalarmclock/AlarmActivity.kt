096db0f63d7654c8604d19240b8e5eef1d9b8e66
Linad <merzlikindaniil97@gmail.com>
2024-10-20 14:53:42 +0300

    git init

---
+++ app/src/main/java/com/example/androidalarmclock/AlarmActivity.kt
@@ -0,0 +1,49 @@
+package com.example.androidalarmclock
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
+
+class AlarmActivity : AppCompatActivity() {
+
+    private lateinit var stopAlarmBTN:Button
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_alarm)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        stopAlarmBTN = findViewById(R.id.stopAlarmBTN)
+        stopAlarmBTN.setOnClickListener {
+            finish()
+            exitProcess(0)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.exit -> {
+                finishAffinity()
+            }
+        }
+        return true
+    }
+}
